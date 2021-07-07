fe7c80057ec5ef0ef56e00f0507b606c43e7d682
LiviaDevolder <liviadevolder@gmail.com>
2021-07-07 10:04:55 -0300

    Aula 5

---
+++ api/models/niveis.js
@@ -0,0 +1,21 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Niveis extends Model {
+
+    static associate(models) {
+      Niveis.hasMany(models.Matriculas, {
+        foreignKey: 'turma_id'
+      })
+    }
+  };
+  Niveis.init({
+    desc_nivel: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Niveis',
+  });
+  return Niveis;
+};
